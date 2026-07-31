d0d669a21c437602893edbe2995f8b2ab37796a0
dartstore <163876009+dartstore@users.noreply.github.com>
2026-07-31 13:19:10 +0300

    Delete backend directory

--- backend/src/notifications/notifications.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { NotificationsController } from './notifications.controller';
-import { PrismaModule } from '../prisma/prisma.module';
-
-@Module({
-  imports: [PrismaModule],
-  controllers: [NotificationsController],
-})
-export class NotificationsModule {}
