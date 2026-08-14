7b56b42806bd0591883e797888b2f15dfd9a1d1e
dartstore <163876009+dartstore@users.noreply.github.com>
2026-08-14 11:11:29 +0300

    Delete backend/src directory

--- backend/src/wallet/wallet.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { WalletController } from './wallet.controller';
-import { PrismaModule } from '../prisma/prisma.module';
-
-@Module({
-  imports: [PrismaModule],
-  controllers: [WalletController],
-})
-export class WalletModule {}
